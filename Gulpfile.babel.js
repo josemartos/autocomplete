938927968eda7938d40b003aded205a0edba3ef4
josemartos <trabajoimperio@gmail.com>
2016-12-17 12:44:39 +0100

    Autocomplete | Improvements

--- Gulpfile.babel.js
+++ Gulpfile.babel.js
@@ -1,12 +1,10 @@
 import gulp from 'gulp';
-import plugins from 'gulp-load-plugins';
 import sass from 'gulp-sass';
 import cleanCss from 'gulp-clean-css';
 import sourcemaps from 'gulp-sourcemaps';
 import uglify from 'gulp-uglify';
 import gutil from 'gulp-util';
 import concat from 'gulp-concat';
-import rename from 'gulp-rename';
 import jshint from 'gulp-jshint';
 import runSequence from 'run-sequence';
 
@@ -41,12 +39,12 @@ gulp.task('compile-sass', () => {
 });
 
 gulp.task('run-jshint', function() {
-  return gulp.src(paths.scripts.source)
-    .pipe(jshint({esversion: 6}))
-    .pipe(jshint.reporter('jshint-stylish', {verbose: true}))
+    gulp.src(paths.scripts.source)
+        .pipe(jshint({esversion: 6}))
+        .pipe(jshint.reporter('jshint-stylish', {verbose: true}))
 });
 
-gulp.task('compile-js', function() {
+gulp.task('compile-scripts', function() {
     browserify({ debug: true })
         .transform(babelify)
         .require("./source/scripts/app.js", { entry: true })
@@ -59,9 +57,9 @@ gulp.task('compile-js', function() {
 gulp.task('watch', () => {
     gulp.watch(paths.sass.source, ['compile-sass']);
     gulp.watch(paths.scripts.source, ['run-jshint']);
-    gulp.watch(paths.scripts.source, ['compile-js']);
+    gulp.watch(paths.scripts.source, ['compile-scripts']);
 });
 
 gulp.task('build', function () {
-    runSequence('compile-sass', 'run-jshint', 'compile-js');
+    runSequence('compile-sass', 'run-jshint', 'compile-scripts');
 });
---
+++ README.md
@@ -0,0 +1,31 @@
+# autocomplete
+
+<<<<<<< HEAD
+Plain JavaScript autocomplete with some ES6 sugar
+
+## description
+
+Created for testing purposes. The auto-complete.js component could be more customisable (e.g. using a class instead of an id, or the used field [name] to filter by).
+
+## setup
+
+install modules
+
+```sh
+npm install
+```
+
+build the project
+
+```sh
+gulp build
+```
+
+Open in a browser
+
+```sh
+web/index.html
+```
+=======
+Vanilla JavaScript autocomplete with some ES6 sugar
+>>>>>>> 398a6f7b19dbba57227ce6c04a6e89d787cbb5d9
